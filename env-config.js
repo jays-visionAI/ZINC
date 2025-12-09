98e7cc14d909245d276e6e86eccae351757e0b36
jays-visionAI <jays@visai.io>
2025-12-10 03:36:46 +0700

    fix: Add env-config.js for deployment
    - Include Google API config for production deployment
    - Remove env-config.js from .gitignore

---
+++ env-config.js
@@ -0,0 +1,8 @@
+/**
+ * Production Environment Configuration
+ * This file is for deployed environments only
+ */
+window.ENV_CONFIG = {
+    GOOGLE_CLIENT_ID: '670347890116-4oo0t76jnmd26ee4b2gdgjvb99a9rqb8.apps.googleusercontent.com',
+    GOOGLE_API_KEY: 'AIzaSyAEi8M_LwD4vAMcX_oJJV6Zq3doUI4P-54'
+};
